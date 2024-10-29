656cbd243bb51e40a9e9b9e44418cd044d866bcb
BeadiestStar64 <beadiestbench64@gmail.com>
2024-10-29 19:26:13 +0900

    v.0.1.4

--- src/main/java/com/github/BeadiestStart64/Minecraft/Plugins/fabrictutorial/block/ModBlocks.java
+++ src/main/java/com/github/BeadiestStart64/Minecraft/Plugins/fabrictutorial/block/ModBlocks.java
@@ -4,6 +4,7 @@ import com.github.BeadiestStart64.Minecraft.Plugins.fabrictutorial.FabricTutoria
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +12,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -21,11 +23,27 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(3f)
                     .requiresTool()));
 
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
+
+    // ブロックを登録
+    // これはワールドでの動作を定義
     private static Block registerBlock(String name, Block block) {
+        // アイテムとして登録
         registerBlockItem(name, block);
+        // ブロックとして登録
         return Registry.register(Registries.BLOCK, Identifier.of(FabricTutorial.MOD_ID, name), block);
     }
 
+    // ブロックを登録
+    // これはインベントリでの動作を定義
     private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(FabricTutorial.MOD_ID, name),
                 new BlockItem(block, new Item.Settings()));
@@ -34,9 +52,14 @@ public class ModBlocks {
     public static void registerModBlocks() {
         FabricTutorial.LOGGER.info("Registering Mod Blocks for " + FabricTutorial.MOD_ID);
 
+        // Createモード時の検知部ブロックタブにアイテムを追加する
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            // 建築タブに以下のアイテムを追加
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
